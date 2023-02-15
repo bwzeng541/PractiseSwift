703eb8708c1c352fdfff9a949b4e2daf942b1dd0
bwzeng541 <283815318@qq.com>
2023-02-15 21:08:07 +0800

    meger

--- PractiseSwift/ViewController.swift
+++ PractiseSwift/ViewController.swift
@@ -11,17 +11,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-<<<<<<< HEAD
          // Do any additional setup after loading the view.
         print("add maingit");
     }
     
     func addmyDevle(){
-        
-=======
-        print("dels all");
-        // Do any additional setup after loading the view.
->>>>>>> parent of acbe7cb (restver)
+    
     }
 
     func addmian(){
