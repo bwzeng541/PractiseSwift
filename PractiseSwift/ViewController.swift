3d0e48556941a3c62f73fb47e156657140782aa6
bwzeng541 <283815318@qq.com>
2023-02-15 14:38:43 +0800

    test2

--- PractiseSwift/ViewController.swift
+++ PractiseSwift/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("sdf");
+        print("secned");
         // Do any additional setup after loading the view.
     }
 
